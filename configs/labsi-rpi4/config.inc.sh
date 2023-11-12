e81a690eeb763f9a9de7c7559ec807863f6c19ec
Stancu Florin <niflostancu@gmail.com>
2023-11-12 16:45:05 +0200

    labsi: use custom kernel (works) + update rpi BL configs

--- configs/labsi-rpi4/config.inc.sh
+++ configs/labsi-rpi4/config.inc.sh
@@ -7,12 +7,17 @@ KERNEL_DEFCONFIG=${KERNEL_DEFCONFIG:-"bcm2711_defconfig"}
 UBOOT_DEFCONFIG=${UBOOT_DEFCONFIG:-"rpi_4_defconfig"}
 
 RPI_FIRMWARE_FILES=(start4.elf fixup4.dat bcm2711-rpi-4-b.dtb
-	overlays/overlay_map.dtb overlays/hat_map.dtb overlays/dwc2.dtbo overlays/disable-bt.dtbo)
+	overlays/overlay_map.dtb overlays/hat_map.dtb overlays/upstream-pi4.dtbo
+	overlays/dwc2.dtbo overlays/disable-bt.dtbo)
 
-EXTRA_PACKAGES=(raspi-firmware linux-image-generic linux-headers-generic linux-libc-dev)
-
-SKIP_BOOT_FILES=y
 RPI_SKIP_IMAGE_GEN=1
+LABSI_USE_COMPILED_KERNEL=1
+if [[ -n "LABSI_USE_COMPILED_KERNEL" ]]; then
+	EXTRA_PACKAGES=()
+else
+	EXTRA_PACKAGES=(raspi-firmware linux-image-generic linux-headers-generic linux-libc-dev)
+	SKIP_BOOT_FILES=y
+fi
 
 function rootfs_install_hook() {
 	# copy custom install scripts to exec. dir
